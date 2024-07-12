7770639671ad1c834d1df80836c728a6122a77c2
jccazes <cazes.jean.christophe@gmail.com>
2024-07-12 09:07:51 +0200

    testing endpoint working

--- data/README.md
+++ data/README.md
@@ -2,3 +2,4 @@ README
 ==============================
 
 This directory stays empty on GitHub but allows the Docker app to download and store data from AWS S3.
+zazie.jpg for testing purposes
--- src/test_api/test_fastapi_main.py
+++ src/test_api/test_fastapi_main.py
@@ -2,14 +2,10 @@ from fastapi.testclient import TestClient
 from api.fastapi_main import app
 from api.fastapi_main import app, lifespan
 import pytest
+from api.utils.resolve_path import resolve_path
 
 # Création de la fixture pour la connexion DuckDB et l'application FastAPI
 # https://stackoverflow.com/questions/75714883/how-to-test-a-fastapi-endpoint-that-uses-lifespan-function
-
-# @pytest.fixture(scope="module")
-# def client():
-#     test_client = TestClient(app, startup=lifespan, shutdown=lifespan)
-#     yield test_client
 
 def test_api():
     
@@ -30,77 +26,33 @@ def test_api():
         login_response = client.post("/token", data={"username": "jc", "password": "jc"})
         token = login_response.json()["access_token"]
         headers = {"Authorization": f"Bearer {token}"}
-        response = client.get("/read_listing/1", headers=headers)
+        
+        with open(resolve_path('data/zazie.jpg'), "rb") as img:
+            files = {"image": ("zazie.jpg", img, "image/jpeg")}
+            params = {"description": "Livre Zazie dans le métro", "designation": "Livre de Raymond Queneau"}
+            
+            response = client.post("/listing_submit", headers=headers, params=params, files=files)
+            
+            assert response.status_code == 200
+            assert "prediction" in response.json()
+        
+        
+        listing_id = response.json().get("listing_id")
+        params = {"listing_id": listing_id, "user_prdtypecode": "1160"}
+        response = client.post("/listing_validate", headers=headers, params=params)
         assert response.status_code == 200
-        assert "designation" in response.json()
-
-# def test_unauthorized_login(client):
-#     """Testing if secured endpoint doesnt work because it has no auth."""
-#     response = client.get("/token")
-#     assert response.status_code == 405
-
-# def test_login_for_access_token(client):
-#     response = client.post("/token", data={"username": "testuser", "password": "testpassword"})
-#     assert response.status_code == 200
-#     assert "access_token" in response.json()
-#     assert response.json()["token_type"] == "bearer"
-
-# def test_read_listing():
-#     # Simuler la connexion pour obtenir le token
-#     login_response = client.post("/token", data={"username": "testuser", "password": "testpassword"})
-#     token = login_response.json()["access_token"]
-#     headers = {"Authorization": f"Bearer {token}"}
-
-#     # Test du endpoint read_listing
-#     response = client.get("/read_listing/1", headers=headers)
-#     assert response.status_code == 200
-#     assert "designation" in response.json()
-
-# def test_delete_listing():
-#     # Simuler la connexion pour obtenir le token
-#     login_response = client.post("/token", data={"username": "testuser", "password": "testpassword"})
-#     token = login_response.json()["access_token"]
-#     headers = {"Authorization": f"Bearer {token}"}
-
-#     # Test du endpoint delete_listing
-#     response = client.delete("/delete_listing/1", headers=headers)
-#     assert response.status_code == 200
-#     assert response.json() == {"message": "Listing deleted successfully"}
-
-# def test_listing_submit():
-#     # Simuler la connexion pour obtenir le token
-#     login_response = client.post("/token", data={"username": "testuser", "password": "testpassword"})
-#     token = login_response.json()["access_token"]
-#     headers = {"Authorization": f"Bearer {token}"}
-
-#     # Test du endpoint listing_submit
-#     with open("path/to/test/image.jpg", "rb") as img:
-#         files = {"image": img}
-#         data = {"description": "test description", "designation": "test designation"}
-#         response = client.post("/listing_submit", headers=headers, data=data, files=files)
-#         assert response.status_code == 200
-#         assert "listing_id" in response.json()
-
-# def test_listing_validate():
-#     # Simuler la connexion pour obtenir le token
-#     login_response = client.post("/token", data={"username": "testuser", "password": "testpassword"})
-#     token = login_response.json()["access_token"]
-#     headers = {"Authorization": f"Bearer {token}"}
-
-#     # Test du endpoint listing_validate
-#     data = {"listing_id": 1, "user_prdtypecode": 123}
-#     response = client.post("/listing_validate", headers=headers, json=data)
-#     assert response.status_code == 200
-#     assert response.json() == {"message": f"Listing 1 validated successfully with user_prdtypecode 123"}
-
-# def test_predict_listing():
-#     # Simuler la connexion pour obtenir le token
-#     login_response = client.post("/token", data={"username": "testuser", "password": "testpassword"})
-#     token = login_response.json()["access_token"]
-#     headers = {"Authorization": f"Bearer {token}"}
-
-#     # Test du endpoint predict_listing
-#     data = {"listing_id": 1}
-#     response = client.post("/predict_listing", headers=headers, json=data)
-#     assert response.status_code == 200
-#     assert "prediction" in response.json()
+        assert "message" in response.json()
+        
+        response = client.get(f"/read_listing/{listing_id}", headers=headers)
+        assert response.status_code == 200
+        response_data = response.json()
+        assert response_data.get("user_prdtypecode") is not None
+        assert response_data.get("model_prdtypecode") is not None
+        
+        params = {"listing_id": 20}
+        response = client.post("/predict_listing", headers=headers, params=params)
+        assert response.status_code == 200
+        assert "prediction" in response.json()
+        
+        response = client.delete(f"/delete_listing/{listing_id}", headers=headers)
+        assert response.status_code == 200
