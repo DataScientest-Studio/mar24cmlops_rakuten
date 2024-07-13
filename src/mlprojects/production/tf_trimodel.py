f1f836b9657060a9a68f12d89451d214ae53e8dd
jccazes <cazes.jean.christophe@gmail.com>
2024-07-13 17:09:40 +0200

    trying new extended class

--- src/mlprojects/production/tf_trimodel.py
+++ src/mlprojects/production/tf_trimodel.py
@@ -18,13 +18,37 @@ import pickle
 
 
 class tf_trimodel:
+    """
+    Base class for a tri-model involving text and image processing using LSTM and VGG16.
+    
+    Attributes:
+        model_type (str): Type of the model (production or staging).
+        version (str): Version of the model.
+        model_name (str): Name of the model.
+        lemmatizer (WordNetLemmatizer): Lemmatizer for text processing.
+        tokenizer (Tokenizer): Tokenizer for text processing.
+        stop_words (set): Set of stop words for text processing.
+        lstm (Model): LSTM model for text prediction.
+        vgg16 (Model): VGG16 model for image prediction.
+        mapper (dict): Mapper for prediction classes.
+        best_weights (list): Weights for aggregating predictions.
+    """
+
     def __init__(self, model_name, version, model_type):
+        """
+        Initialize the tf_trimodel class.
+        
+        Args:
+            model_name (str): Name of the model.
+            version (str): Version of the model.
+            model_type (str): Type of the model (production or staging).
+        """
         self.model_type = model_type
         self.version = version
         if model_name is None:
             self.model_name = type(
                 self
-            ).__name__  # Utilise le nom de la classe comme nom par défaut
+            ).__name__  # Use the class name as the default name
         else:
             self.model_name = model_name
         self.lemmatizer = None
@@ -39,23 +63,23 @@ class tf_trimodel:
         self.load_model_utils()
 
     def get_model_path(self):
-        # Determine folder type based on model_type
+        """
+        Get the model path based on model type and version.
+        
+        Returns:
+            str: Path to the model.
+        """
         folder = (
             "production_model" if self.model_type == "production" else "staging_models"
         )
 
-        # Determine version folder
-        # if self.version == 'latest':
-        #     versions = sorted(os.listdir(resolve_path(f'models/{folder}/{self.model_name}/')))
-        #     version = versions[-1]
-        # else:
-        #     version = self.version
-
-        # Construct base path
         base_path = resolve_path(f"models/{folder}/{self.model_name}/{self.version}/")
         return base_path
 
     def load_txt_utils(self):
+        """
+        Load text utilities including tokenizer, stop words, and LSTM model.
+        """
         nltk.download("punkt")
         nltk.download("stopwords")
         nltk.download("wordnet")
@@ -73,12 +97,18 @@ class tf_trimodel:
         )
 
     def load_img_utils(self):
+        """
+        Load image utilities including VGG16 model.
+        """
         model_path = self.get_model_path()
         self.vgg16 = keras.models.load_model(
             os.path.join(model_path, "best_vgg16_model.keras")
         )
 
     def load_model_utils(self):
+        """
+        Load model utilities including mapper and best weights.
+        """
         model_path = self.get_model_path()
 
         with open(os.path.join(model_path, "mapper.json"), "r") as json_file:
@@ -88,6 +118,15 @@ class tf_trimodel:
             self.best_weights = pickle.load(file)
 
     def process_txt(self, text):
+        """
+        Process the input text for prediction.
+        
+        Args:
+            text (str): Input text.
+        
+        Returns:
+            np.array: Processed text sequence.
+        """
         if text is not None:
             text = re.sub(r"[^a-zA-Z]", " ", text)
             words = word_tokenize(text.lower())
@@ -105,30 +144,85 @@ class tf_trimodel:
         return None
 
     def path_to_img(self, img_path):
+        """
+        Load an image from a file path.
+        
+        Args:
+            img_path (str): Path to the image file.
+        
+        Returns:
+            PIL.Image: Loaded image.
+        """
         img = load_img(img_path, target_size=(224, 224, 3))
         return img
 
     def byte_to_img(self, file):
+        """
+        Load an image from a byte stream.
+        
+        Args:
+            file (bytes): Byte stream of the image.
+        
+        Returns:
+            PIL.Image: Loaded image.
+        """
         img = load_img(BytesIO(file), target_size=(224, 224, 3))
         return img
 
     def process_img(self, img):
+        """
+        Process the input image for prediction.
+        
+        Args:
+            img (PIL.Image): Input image.
+        
+        Returns:
+            np.array: Processed image array.
+        """
         img_array = img_to_array(img)
         img_array = preprocess_input(img_array)
         return img_array
 
     def predict_text(self, text_sequence):
+        """
+        Predict the class of the input text sequence using LSTM model.
+        
+        Args:
+            text_sequence (np.array): Processed text sequence.
+        
+        Returns:
+            tuple: Predicted class and probability.
+        """
         probability = self.lstm.predict([text_sequence], verbose=0)
         pred = np.argmax(probability)
         return int(self.mapper[str(pred)]), probability
 
     def predict_img(self, img_array):
+        """
+        Predict the class of the input image array using VGG16 model.
+        
+        Args:
+            img_array (np.array): Processed image array.
+        
+        Returns:
+            tuple: Predicted class and probability.
+        """
         images = tf.convert_to_tensor([img_array], dtype=tf.float32)
         probability = self.vgg16.predict([images], verbose=0)
         pred = np.argmax(probability)
         return int(self.mapper[str(pred)]), probability
 
     def agg_prediction(self, txt_prob, img_prob):
+        """
+        Aggregate predictions from text and image models.
+        
+        Args:
+            txt_prob (np.array): Probability distribution from text model.
+            img_prob (np.array): Probability distribution from image model.
+        
+        Returns:
+            int: Aggregated predicted class.
+        """
         concatenate_proba = (
             self.best_weights[0] * txt_prob + self.best_weights[1] * img_prob
         )
@@ -136,6 +230,16 @@ class tf_trimodel:
         return int(self.mapper[str(pred)])
 
     def predict(self, designation, image):
+        """
+        Predict the class based on both text and image inputs.
+        
+        Args:
+            designation (str): Input text.
+            image (str or bytes): Input image path or byte stream.
+        
+        Returns:
+            int: Predicted class.
+        """
         text_sequence = self.process_txt(designation)
         if isinstance(image, str):  # If image is a path
             img = self.path_to_img(image)
@@ -151,7 +255,6 @@ class tf_trimodel:
 
         agg_pred = self.agg_prediction(txt_prob, img_prob)
         return agg_pred
-
 
 # Utilisation de la classe Model
 
---
+++ src/mlprojects/production/tf_trimodel_extended.py
@@ -0,0 +1,132 @@
+from mlprojects.production.tf_trimodel import tf_trimodel
+import numpy as np 
+import os 
+import pickle
+from scipy.optimize import minimize
+from api.utils.resolve_path import resolve_path
+from api.utils.make_db import process_listing
+
+class tf_trimodel_extended(tf_trimodel):
+    """
+    Extended class of tf_trimodel with additional methods to retrain the model 
+    and the aggregation layer.
+    """
+
+    def __init__(self, model_name, version, model_type):
+        """
+        Initialize the tf_trimodel_extended class.
+        
+        Args:
+            model_name (str): Name of the model.
+            version (str): Version of the model.
+            model_type (str): Type of the model (production or staging).
+        """
+        super().__init__(model_name, version, model_type)
+        
+    def retrain_model(self, new_text_data, new_image_data, new_labels, epochs=10, batch_size=32):
+        """
+        Retrain the models with new data.
+        
+        Args:
+            new_text_data (list of str): New text data for retraining.
+            new_image_data (list of str or bytes): New image data for retraining.
+            new_labels (list of int): New labels for retraining.
+            epochs (int): Number of epochs for retraining.
+            batch_size (int): Batch size for retraining.
+        """
+        # Update tokenizer with new text data
+        self.tokenizer.fit_on_texts(new_text_data)
+        
+        # Save the updated tokenizer
+        tokenizer_json = self.tokenizer.to_json()
+        model_path = self.get_model_path()
+        with open(os.path.join(model_path, "tokenizer_config.json"), "w", encoding="utf-8") as json_file:
+            json_file.write(tokenizer_json)
+        
+        # Process the new text data
+        processed_texts = [self.process_txt(text) for text in new_text_data]
+        text_sequences = np.array([sequence[0] for sequence in processed_texts])
+        
+        # Process the new image data
+        processed_images = [self.process_img(self.path_to_img(img)) if isinstance(img, str) else self.process_img(self.byte_to_img(img)) for img in new_image_data]
+        image_arrays = np.array(processed_images)
+        
+        # Transform the labels
+        new_labels = np.array(new_labels)
+        
+        # Compile the LSTM text model
+        self.lstm.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+        
+        # Retrain the LSTM text model
+        self.lstm.fit(text_sequences, new_labels, epochs=epochs, batch_size=batch_size, verbose=1)
+        
+        # Compile the VGG16 image model
+        self.vgg16.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+        
+        # Retrain the VGG16 image model
+        self.vgg16.fit(image_arrays, new_labels, epochs=epochs, batch_size=batch_size, verbose=1)
+        
+        # Save the retrained models
+        self.lstm.save(os.path.join(model_path, "retrained_lstm_model.keras"))
+        self.vgg16.save(os.path.join(model_path, "retrained_vgg16_model.keras"))
+
+    def retrain_agg_layer(self, new_text_data, new_image_data, new_labels, epochs=10, batch_size=32):
+        """
+        Retrain the aggregation layer with new data.
+        
+        Args:
+            new_text_data (list of str): New text data for retraining.
+            new_image_data (list of str or bytes): New image data for retraining.
+            new_labels (list of int): New labels for retraining.
+            epochs (int): Number of epochs for retraining.
+            batch_size (int): Batch size for retraining.
+        """
+        # Process the new text data
+        processed_texts = [self.process_txt(text) for text in new_text_data]
+        text_sequences = np.array([sequence[0] for sequence in processed_texts])
+        
+        # Process the new image data
+        processed_images = [self.process_img(self.path_to_img(img)) if isinstance(img, str) else self.process_img(self.byte_to_img(img)) for img in new_image_data]
+        image_arrays = np.array(processed_images)
+        
+        # Transform the labels
+        new_labels = np.array(new_labels)
+
+        # Get the probabilities from the text and image models
+        text_probs = np.array([self.predict_text(seq)[1] for seq in text_sequences])
+        image_probs = np.array([self.predict_img(img)[1] for img in image_arrays])
+
+        # Define the objective function for optimization
+        def objective(weights):
+            weighted_probs = weights[0] * text_probs + weights[1] * image_probs
+            predictions = np.argmax(weighted_probs, axis=1)
+            accuracy = np.mean(predictions == new_labels)
+            return -accuracy  # Minimize negative accuracy
+
+        # Initial weights
+        initial_weights = self.best_weights
+        
+        # Optimize the weights
+        result = minimize(objective, initial_weights, method='Nelder-Mead')
+        optimized_weights = result.x
+
+        # Update the best weights
+        self.best_weights = optimized_weights
+        
+        # Save the updated best weights
+        model_path = self.get_model_path()
+        with open(os.path.join(model_path, "best_weights.pkl"), "wb") as file:
+            pickle.dump(self.best_weights, file)
+        
+# Exemple d'utilisation de la classe étendue
+#tf_model_extended = tf_trimodel_extended(model_name='tf_trimodel', version='20240708_18-15-54', model_type='production')
+
+listing_df = process_listing(resolve_path('data/X_train.csv'),resolve_path('data/Y_train.csv'))
+listing_df = listing_df.head(1000)
+
+new_text_data = list(listing_df['designation'])
+new_image_data = listing_df.apply(lambda row: resolve_path(f"image_{row['imageid']}_product_{row['productid']}"), axis=1).tolist()
+new_labels = list(listing_df['user_prdtypecode'])
+
+tf_model_extended.retrain_model(new_text_data, new_image_data, new_labels)
+tf_model_extended.retrain_agg_layer(new_text_data, new_image_data, new_labels)
